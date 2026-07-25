d959dbab3b4dfce4cfe30e7d3fcc00319287fdcf
Roy7721 <s2010724129@ru.ac.bd>
2026-07-26 03:29:15 +0600

    token_issue_solved_through_the_entire_pipeline

--- src/models/evaluation.py
+++ src/models/evaluation.py
@@ -18,13 +18,13 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from scipy.sparse import hstack, csr_matrix
+from dotenv import load_dotenv
+load_dotenv()
 
 
 
 
 
-os.environ["MLFLOW_TRACKING_USERNAME"] = "Roy7721"
-os.environ["MLFLOW_TRACKING_PASSWORD"] = "bc912b7d58bd2aca05abdc192e010414493a3886"
 mlflow.set_tracking_uri("https://dagshub.com/Roy7721/yt_comment_analysis.mlflow")
 mlflow.set_experiment("dvc-pipeline-runs#1")
 
--- src/models/register_model.py
+++ src/models/register_model.py
@@ -5,15 +5,10 @@ import pickle
 import mlflow
 import logging
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
-# Set up MLflow tracking URI
-#mlflow.set_tracking_uri("http://ec2-54-196-109-131.compute-1.amazonaws.com:5000/")
 
-
-# MLflow / DagShub tracking setup
-
-os.environ["MLFLOW_TRACKING_USERNAME"] = "Roy7721"
-os.environ["MLFLOW_TRACKING_PASSWORD"] = "bc912b7d58bd2aca05abdc192e010414493a3886"
 mlflow.set_tracking_uri("https://dagshub.com/Roy7721/yt_comment_analysis.mlflow")
 #mlflow.set_experiment("LogisticRegression HP Tuning with Custom Features")
 
